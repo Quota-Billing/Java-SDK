c50df675ef0d30e262ea6c5399913a431ae7ac82
weinbec <weinbec@rose-hulman.edu>
2017-10-05 22:06:00 -0400

    Add user feature

---
+++ src/main/java/edu/rosehulman/quota/javasdk/Partner.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.quota.javasdk;
+
+class Partner {
+
+  private String PartnerId;
+
+  Partner(String apiKey) {
+    // TODO: Make a call to Quota server to get a Partner for the apiKey and set the
+    // fields in this class
+  }
+
+  String getPartnerId() {
+    return PartnerId;
+  }
+}
---
+++ src/test/java/edu/rosehulman/quota/javasdk/ProductTest.java
@@ -0,0 +1,35 @@
+package edu.rosehulman.quota.javasdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ QuotaClient.class, Partner.class, User.class })
+public class ProductTest {
+
+  @Test
+  public void addUserTest() throws Exception {
+    // Mock non-static classes
+    Partner partner = Mockito.mock(Partner.class);
+    User user = Mockito.mock(User.class);
+
+    // Create object we are testing
+    Product product = new Product(partner, "the_product_id");
+
+    // Statically mock the QuotaClient because it is a singleton
+    mockStatic(QuotaClient.class);
+    QuotaClient quotaClient = Mockito.mock(QuotaClient.class);
+    when(QuotaClient.getInstance()).thenReturn(quotaClient);
+    when(quotaClient.addUser(partner, product, user)).thenReturn(true);
+
+    // Assert
+    assertEquals(true, product.addUser(user));
+  }
+}

ac66f029030736257247033d8b52bfcd8f9e6586
Jack Hallam <hallamja@rose-hulman.edu>
2017-10-17 20:53:12 -0400

    temp set partner id

--- src/main/java/edu/rosehulman/quota/javasdk/Partner.java
+++ src/main/java/edu/rosehulman/quota/javasdk/Partner.java
@@ -2,14 +2,15 @@ package edu.rosehulman.quota.javasdk;
 
 class Partner {
 
-  private String PartnerId;
+  private String partnerId;
 
   Partner(String apiKey) {
     // TODO: Make a call to Quota server to get a Partner for the apiKey and set the
     // fields in this class
+    partnerId = "partnerId";
   }
 
   String getPartnerId() {
-    return PartnerId;
+    return partnerId;
   }
 }
